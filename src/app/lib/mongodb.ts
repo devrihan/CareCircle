3ad07ff8082c19196eeb1bd85d46456be8ebdf9c
devrihan <rihandav2004@gmail.com>
2025-06-19 11:05:56 +0530

    cahnges5

--- src/app/lib/mongodb.ts
+++ src/app/lib/mongodb.ts
@@ -1,31 +1,12 @@
-// import mongoose from 'mongoose';
 
-// const MONGODB_URI = process.env.MONGODB_URI!;
-
-// if (!MONGODB_URI) throw new Error('Please define MONGODB_URI in .env');
-
-// declare global {
-//   var mongoose: {
-//     conn: any;
-//     promise: Promise<any> | null;
-//   } | undefined;
-// }
-
-// let cached = global.mongoose || { conn: null, promise: null };
-
-// export async function dbConnect() {
-//   if (cached.conn) return cached.conn;
-
-//   if (!cached.promise) {
-//     cached.promise = mongoose.connect(MONGODB_URI).then((conn) => conn);
-//   }
-
-//   cached.conn = await cached.promise;
-//   return cached.conn;
-// }
 import mongoose, { Connection } from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI!;
+console.log('MONGODB_URI:', MONGODB_URI);
+console.log('Test');
+
+
+
 if (!MONGODB_URI) throw new Error('Please define MONGODB_URI in .env');
 
 declare global {
