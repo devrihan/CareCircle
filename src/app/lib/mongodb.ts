7e4217e0196aa69cd9dfe4f162375496cd3fce40
devrihan <rihandav2004@gmail.com>
2025-06-19 11:50:20 +0530

    hehe2

--- src/app/lib/mongodb.ts
+++ src/app/lib/mongodb.ts
@@ -2,12 +2,7 @@
 import mongoose, { Connection } from 'mongoose';
 
 const MONGODB_URI = "mongodb+srv://rihandav2004:uEm0F7HshKTBmmHS@cluster0.sx7ugdv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-console.log('MONGODB_URI:', process.env.MONGODB_URI);
 console.log('Test');
-
-
-
-if (!MONGODB_URI) throw new Error('Please define MONGODB_URI in .env');
 
 declare global {
   var mongoose: {
